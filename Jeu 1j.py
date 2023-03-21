c6f150eb1960d74bc906626a0dd47e2f644fb380
uvsq22107184 <91122812+uvsq22107184@users.noreply.github.com>
2023-03-21 09:10:18 +0000

    première partie

---
+++ Jeu 1j.py
@@ -0,0 +1,17 @@
+import random
+import pygame
+
+# Initialisation de Pygame
+pygame.init()
+
+# Définition de la taille de la fenêtre
+WIDTH = 400
+HEIGHT = 600
+
+# Définition des couleurs utilisées dans le jeu
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
