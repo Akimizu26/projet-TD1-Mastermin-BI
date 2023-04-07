31a20d582ac49fda541ef471007372901cc728a7
Akimizu26 <114061870+Akimizu26@users.noreply.github.com>
2023-04-07 11:57:50 +0200

    V0.1 Title, SpinBox
    -Ajout du titre « Mastermind »
    -Ajoute du Widget Combox

--- Newmastermind.py
+++ Newmastermind.py
@@ -1,4 +1,13 @@
-from tkinter import Tk
+from tkinter import Label, Tk
+from tkinter.ttk import Combobox
 
 root = Tk()
+root.title("Mastermind TD01")
+root.config(bg='blue')
+
+Label(root, text='Choisissez un nombre:').pack()
+my_box = Combobox(root, values=(2, 4, 6, 8))
+my_box.current(2)
+my_box.pack()
+
 root.mainloop()
