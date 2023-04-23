989cd2600cc730885c57272dd50f41d4fec8b77f
Enzo <114061870+Akimizu26@users.noreply.github.com>
2023-04-23 22:12:28 +0200

    Système de sauvegarde
    -Ajout d'un système de sauvegarde avec un java script: le jeu se sauvegarde sur un ficher data.json
    Le chemin d'accès au fichier JSON pour stocker les données du jeu et le nombre maximum de coups permis dans le jeu.

--- Newmastermind.py
+++ Newmastermind.py
@@ -1,8 +1,10 @@
 #Ici on va importer toutes les libraires dont on a besoin pour le projet
 import tkinter as tk
 import random
+import json
 
 #On donne un nom à notre fenêtre principale et on la dimensionne
+PATH = "data.json"
 
 NB_OF_GUESS = 11
 TMP = NB_OF_GUESS
@@ -12,4 +14,3 @@ COLOR_PADDING = 50
 
 WIDTH = CODE_LENGTH*90
 HEIGHT = NB_OF_GUESS*55+70
-
