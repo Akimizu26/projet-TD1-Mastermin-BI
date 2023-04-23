8c580f30ba4f7bdd999ecccc5627c0eeba9b6493
Enzo <114061870+Akimizu26@users.noreply.github.com>
2023-04-23 22:24:01 +0200

    Couleur du jeu
    Ajout de:
    -Des listes à deux dimensions pour stocker les choix de couleurs de chaque joueur et les indices correspondants.
    
    -Une fonction pour permettre à l'utilisateur de sélectionner les couleurs.
    
    -Une fonction pour permettre à l'utilisateur de désélectionner les couleurs.
    
    -Une fonction pour définir la couleur d'un bouton.
    
    -Une fonction pour sélectionner la position actuelle de la sélection.
    
    -Une fonction pour changer la couleur sélectionnée.

--- Newmastermind.py
+++ Newmastermind.py
@@ -17,3 +17,20 @@ HEIGHT = NB_OF_GUESS*55+70
 
 NB_PLAYER = 1
 CODED_MESSAGE = []
+
+#On donne le nom des couleurs pour le jeu
+basecolors = ['white','green','red','maroon1','gold','dark orange','dodger blue']
+color_indice = ['black','white']
+
+color_button = "#613a19"
+position = 0
+
+
+row = 0
+cpos = 0
+
+selectColors = []
+
+colorpicks = [[-1 for i in range(CODE_LENGTH)] for j in range(NB_OF_GUESS)]
+indice = [[-1 for i in range(CODE_LENGTH)] for j in range(NB_OF_GUESS)]
+
